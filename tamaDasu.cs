9f2f29c1d786a0c5efb1568e9b19d9f1337e4dbc
kiryu1120 <138645777+kiryu1120@users.noreply.github.com>
2024-05-31 10:31:34 +0900

    Update tamaDasu.cs

--- tamaDasu.cs
+++ tamaDasu.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//S Playerの攻撃
 public class tamaDasu : MonoBehaviour
 {
     // Start is called before the first frame update
