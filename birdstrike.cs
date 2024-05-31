f62426906ee8a0d16afa61c55503c5210aa0173c
kiryu1120 <138645777+kiryu1120@users.noreply.github.com>
2024-05-31 10:29:14 +0900

    Update birdstrike.cs

--- birdstrike.cs
+++ birdstrike.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//ABossの玉
 public class birdstrike : MonoBehaviour
 {
     // Start is called before the first frame update
